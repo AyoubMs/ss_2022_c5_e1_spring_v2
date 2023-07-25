f8b9d9a3ccb6de94053feabdfeef1aaa80564440
AyoubMS <chahayb563@gmail.com>
2023-07-25 21:37:29 +0200

    add

---
+++ src/main/java/com/example/ss_2022_c5_e1_spring_v2/Ss2022C5E1SpringV2Application.java
@@ -0,0 +1,13 @@
+package com.example.ss_2022_c5_e1_spring_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2022C5E1SpringV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss2022C5E1SpringV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ss_2022_c5_e1_spring_v2/Ss2022C5E1SpringV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss_2022_c5_e1_spring_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2022C5E1SpringV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
